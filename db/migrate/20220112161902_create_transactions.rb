3cb527294f86fb296a66b03cf4608574778a5992
JM Arenas <rns.jey@gmail.com>
2022-01-13 00:31:00 +0800

    Built Transactions table structure

--- db/migrate/20220112161902_create_transactions.rb
+++ db/migrate/20220112161902_create_transactions.rb
@@ -1,6 +1,16 @@
 class CreateTransactions < ActiveRecord::Migration[6.0]
   def change
     create_table :transactions do |t|
+      t.string :kind
+      t.string :symbol
+      t.float :unit_price
+      t.float :amount
+      t.float :total_price
+      t.float :fee
+      t.float :final_price
+      t.string :currency
+      t.float :status
+      t.integer :user_id
 
       t.timestamps
     end
