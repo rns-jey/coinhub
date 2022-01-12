235899e2b48f78b72c8168eeb089ed49e4f5c890
JM Arenas <rns.jey@gmail.com>
2022-01-12 22:18:52 +0800

    Add new coin to crypto portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,31 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+    @cryptos = Cryptocurrency.pluck(:name) - Portfolio.pluck(:name)
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    @crypto = Cryptocurrency.where(name: @portfolio.name).first
+
+    @portfolio.symbol = @crypto.symbol
+    @portfolio.slug = @crypto.slug
+    @portfolio.amount = 0.00
+    @portfolio.user_id = current_user.id
+
+    if @portfolio.save
+      redirect_to portfolios_path
+    else
+      render :new, status: :unprocessable_entity
+     end
+  end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(:name, :symbol, :slug, :amount, :status, :user_id)
+    end
+
 end
