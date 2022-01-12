31d7e4e0a19155e69916c4a1186bf69d1ea7eedb
JM Arenas <rns.jey@gmail.com>
2022-01-12 17:46:16 +0800

    Built cryptocurrency table structure

--- db/migrate/20220112094402_create_cryptocurrencies.rb
+++ db/migrate/20220112094402_create_cryptocurrencies.rb
@@ -1,7 +1,12 @@
 class CreateCryptocurrencies < ActiveRecord::Migration[6.0]
   def change
     create_table :cryptocurrencies do |t|
-
+      t.string :name
+      t.string :kind
+      t.string :symbol
+      t.string :slug
+      t.boolean :is_active
+      
       t.timestamps
     end
   end
