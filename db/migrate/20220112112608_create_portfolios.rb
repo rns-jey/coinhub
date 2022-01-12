242c6c4ef7f15a273508b144e412acdbc2b7a9de
JM Arenas <rns.jey@gmail.com>
2022-01-12 19:29:07 +0800

    Built Portfolio table structure

--- db/migrate/20220112112608_create_portfolios.rb
+++ db/migrate/20220112112608_create_portfolios.rb
@@ -1,7 +1,13 @@
 class CreatePortfolios < ActiveRecord::Migration[6.0]
   def change
     create_table :portfolios do |t|
-
+      t.string :name
+      t.string :symbol
+      t.string :slug
+      t.float :amount
+      t.float :status
+      t.integer :user_id
+      
       t.timestamps
     end
   end
