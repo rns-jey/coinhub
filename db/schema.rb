4143af32261b334c5a77ce6637d073df30d5c324
JM Arenas <rns.jey@gmail.com>
2022-01-14 23:09:16 +0800

    Removed admins table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_133354) do
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_01_12_161902) do
 
   create_table "cryptocurrencies", force: :cascade do |t|
     t.string "name"
@@ -58,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_133354) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "role", default: "user", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
