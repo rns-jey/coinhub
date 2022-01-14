d59058241727a0d5db4baa735ec9c9c5677050ba
JM Arenas <rns.jey@gmail.com>
2022-01-14 23:15:03 +0800

    Override user approval so it won't allow any user to approve itself by adding an input tag in the registrations form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :portfolios
-         has_many :transactions
+  has_many :portfolios
+  has_many :transactions
+
+  def active_for_authentication? 
+    super && approved? 
+  end 
+    
+  def inactive_message 
+    approved? ? super : :not_approved
+  end
 end
