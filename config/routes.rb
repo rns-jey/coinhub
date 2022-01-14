217d7b15bf7e4eda1a57b505ce9e27045f94a6ed
JM Arenas <rns.jey@gmail.com>
2022-01-15 02:06:32 +0800

    Beautify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   namespace :admins do
     get 'dashboard/index'
   end
+
   namespace :users do
     get 'dashboard/index'
   end
+  
   devise_for :admins
   devise_for :users
-  get '/admins/approvals', to: 'admins#approvals'
+
+  #get '/admins/approvals', to: 'admins#approvals'
   
   root 'home#index'
   resources :portfolios
