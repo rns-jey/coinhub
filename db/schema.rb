fa2a129d1bc30c260bc939c73b5bda978813a683
JM Arenas <rns.jey@gmail.com>
2022-01-14 21:38:39 +0800

    Migrate admins migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_161902) do
+ActiveRecord::Schema.define(version: 2022_01_14_133354) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "cryptocurrencies", force: :cascade do |t|
     t.string "name"
