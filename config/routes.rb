581c3df4354eba5585e54c26ef295f12f3f70fb3
JM Arenas <rns.jey@gmail.com>
2022-01-14 23:50:33 +0800

    Turn scoped views on

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  resources :admins, only: [:new, :create]
-
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
-
-  devise_for :users
-  resources :users
+  devise_for :users, :path => 'users'
+  
   root 'home#index'
   resources :portfolios
   resources :transactions
