693028394103f4005f722b5e9282eb0d0193bd4d
JM Arenas <rns.jey@gmail.com>
2022-01-15 02:03:52 +0800

    Generate dashboard controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :users do
+    get 'dashboard/index'
+  end
   devise_for :admins
   devise_for :users
   get '/admins/approvals', to: 'admins#approvals'
