9f7e27364b37e0a69ea553d3c42bb0da7b9e4989
JM Arenas <rns.jey@gmail.com>
2022-01-14 23:13:50 +0800

    Removed user roles and added approved status

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2022_01_12_161902) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "role", default: "user", null: false
+    t.boolean "approved", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
