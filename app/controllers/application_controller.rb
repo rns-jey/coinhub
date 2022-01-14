8263dff02a2dd8837a1f6db8b228ebadec509826
JM Arenas <rns.jey@gmail.com>
2022-01-15 05:03:30 +0800

    Added devise params firstname lastname country

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname country])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname country])
+  end
+
   # def after_sign_in_path_for(resource)
   #   if resource.instance_of?(Admin)
   #     admins_approvals_path
