84db0ca8ba4606c8fb6e10fffb1bddc0f49267dc
JM Arenas <rns.jey@gmail.com>
2022-01-15 08:39:04 +0800

    RSpec cleanup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'When signing in, user' do
+    it 'is not allowed without email' do
+      user = described_class.new(email: nil, firstname: 'Jane', lastname: 'Doe', password: 'password')
+
+      expect(user).not_to be_valid
+    end
+
+
+    it 'is not allowed without password' do
+      user = described_class.new(email: 'john.doe@coinhub.com', firstname: 'Jane', lastname: 'Doe', password: nil)
+
+      expect(user).not_to be_valid
+    end
+  end
 end
